73accc0744e4e63bd6b0352028e011d6d88173f1
Clement Farabet <clement.farabet@gmail.com>
2013-08-19 04:26:21 +0000

    File IO.

---
+++ test-fileio.lua
@@ -0,0 +1,13 @@
+local async = dofile('async.lua')
+
+async.fs.readFile('test-fileio.lua', function(file)
+   print(file)
+   async.fs.writeFile('test-fileio.lua.backup', file, function(err)
+      print(err)
+   end)
+end)
+
+async.fs.stat('test-fileio.lua', function(stats)
+   print(stats)
+end)
+async.go()
