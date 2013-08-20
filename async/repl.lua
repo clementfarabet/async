ea97340ef8f4d24b21c17ebb0237d8e784890be7
Clement Farabet <clement.farabet@gmail.com>
2013-08-20 23:54:57 +0000

    Colors.

--- async/repl.lua
+++ async/repl.lua
@@ -153,5 +153,37 @@ function repl.connect(domain)
    end)
 end
 
+-- useful: colors
+repl.colors = {
+   none = '\27[0m',
+   black = '\27[0;30m',
+   red = '\27[0;31m',
+   green = '\27[0;32m',
+   yellow = '\27[0;33m',
+   blue = '\27[0;34m',
+   magenta = '\27[0;35m',
+   cyan = '\27[0;36m',
+   white = '\27[0;37m',
+   Black = '\27[1;30m',
+   Red = '\27[1;31m',
+   Green = '\27[1;32m',
+   Yellow = '\27[1;33m',
+   Blue = '\27[1;34m',
+   Magenta = '\27[1;35m',
+   Cyan = '\27[1;36m',
+   White = '\27[1;37m',
+   _black = '\27[40m',
+   _red = '\27[41m',
+   _green = '\27[42m',
+   _yellow = '\27[43m',
+   _blue = '\27[44m',
+   _magenta = '\27[45m',
+   _cyan = '\27[46m',
+   _white = '\27[47m'
+}
+function repl.colorize(str,color)
+   return repl.colors[color] .. str .. repl.colors.none
+end
+
 -- return 
 return repl
