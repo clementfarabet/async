8927ce0a55eed65cefbf417338723fd74735adaf
Clement Farabet <clement.farabet@gmail.com>
2013-08-20 21:21:31 +0000

    Json server uses \n as a default separator.

--- async/json.lua
+++ async/json.lua
@@ -13,13 +13,13 @@ local _,json = pcall(require,'cjson')
 
 function json.listen(domain, cb)
    tcp.listen(domain, function(client)
-      client.onsplitdata('\0', function(req)
+      client.onsplitdata('\n', function(req)
          -- decode json:
          req = json.decode(req)
 
          -- call user handle:
          cb(req, function(res)
-            res = json.encode(res) .. '\0'
+            res = json.encode(res) .. '\n'
             client.write(res)
          end)
       end)
@@ -29,14 +29,14 @@ end
 function json.connect(domain, cb)
    tcp.connect(domain, function(client)
       client.send = function(tbl)
-         local req = json.encode(tbl) .. '\0'
+         local req = json.encode(tbl) .. '\n'
          client.write(req)
       end
       local receive
       client.receive = function(f)
          receive = f
       end
-      client.onsplitdata('\0', function(req)
+      client.onsplitdata('\n', function(req)
          -- decode json:
          req = json.decode(req)
 
