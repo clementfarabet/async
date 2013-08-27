07638af1d09b773e36f44360cfa4ee8a9e19993d
Clement Farabet <clement.farabet@gmail.com>
2013-08-27 15:25:34 +0000

    Added a synchronous readsplit(s) function.

--- async/handle.lua
+++ async/handle.lua
@@ -1,9 +1,15 @@
 -- c lib / bindings for libuv
 local uv = require 'luv'
 
+-- we need penlight for a few convenience functions
+require 'pl'
+
 -- make handle out of uv client
 local function handle(client)
+   -- handle wraper:
    local h = {}
+
+   -- common read/write abstractions:
    h.ondata = function(cb)
       client.ondata = function(self,data)
          if cb then cb(data) end
@@ -29,10 +35,10 @@ local function handle(client)
          if cb then cb() end
       end)
    end
+
    -- convenience function to split a stream,
    -- and call a callback each time a full split is found
    h.onsplitdata = function(limit,cb)
-      require 'pl' -- TODO: replace this dep, only useful for split()
       local fullpacket = {}
       h.ondata(function(chunk)
          local chunks = stringx.split(chunk,limit)
@@ -46,20 +52,64 @@ local function handle(client)
          end
       end)
    end
+
    -- provide a synchronous read:
    h.read = function()
-      local data
+      -- get coroutine:
       local co = coroutine.running()
       if not co then
          print('read() can only be used within a fiber(function() client.read() end) context')
          return nil
       end
+
+      -- read data, and resume coroutine once done
+      local data
       h.ondata(function(d)
          data = d
+         client.ondata = nil
          coroutine.resume(co)
       end)
+
+      -- yield...
       coroutine.yield()
+
+      -- coroutine has been resumed, data is available
       return data
+   end
+
+   -- synchronous readsplit:
+   -- TODO: this function seems to be 100% correct, but given the way
+   -- it's constructed, it implies that the user really knows what he's doing:
+   -- if readsplit(split) is called with the same split symbol over and over,
+   -- then it will be ok all the time. If the split symbol changes, results will
+   -- be unpredictable, because of the buffering.
+   local lines = {}
+   local buffer = {}
+   h.readsplit = function(split)
+      split = split or '\n'
+      if #lines > 0 then
+         local line = lines[1]
+         lines = tablex.sub(lines,2,#lines)
+         return line
+      end
+      while true do
+         local res = h.read()
+         local chunks = stringx.split(res,split)
+         for i,chunk in ipairs(chunks) do
+            if i == 0 then
+               table.insert(buffer,chunk)
+               local line = table.concat(buffer)
+               table.insert(lines,line)
+               buffer = {}
+            elseif i < #chunks then
+               table.insert(lines,chunk)
+            else
+               table.insert(buffer,chunk)
+            end
+         end
+         break
+      end
+      return h.readsplit(split)
    end
 
    return h
--- tests/test-tcp-client-sync.lua
+++ tests/test-tcp-client-sync.lua
@@ -1,14 +1,43 @@
 local async = require 'async'
 
+local test = ''
+for i = 1,10000 do
+   test = test .. 'something\n'
+end
+
 local client = async.tcp.connect('tcp://127.0.0.1:8483/', function(client)
    async.fiber(function()
       client.write('test')
       local res = client.read()
       print('received, sync: ', res)
 
-      client.write('test')
-      local res = client.read()
-      print('received, sync: ', res)
+      client.write(test, function(s)
+         print('wrote ' .. #test .. ' bytes')
+      end)
+      
+      local read = ''
+      while true do
+         local res = client.read()
+         read = read .. res
+         if #read == #test then
+            break
+         end
+      end
+      print('read ' .. #read .. ' bytes') 
+
+      print('read==wrote:', read==test)
+
+      client.write('line1\nline2\nsomething long\n')
+      for i = 1,3 do
+         local line = client.readsplit('\n')
+         print('read one line: ' .. line)
+      end
+      client.write('line1\nline2\nsomething long\n')
+      client.write('line1\nline2\nsomething long\n')
+      for i = 1,6 do
+         local line = client.readsplit('\n')
+         print('read one line: ' .. line)
+      end
 
       client.close()
    end)
