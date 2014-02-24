b1bdeb40180398c3078736db8665f4ac4c6645da
Conall <=>
2014-02-24 17:44:12 -0500

    json server leaks mem

--- tests/test-json-server.lua
+++ tests/test-json-server.lua
@@ -6,6 +6,9 @@ async.json.listen({host='0.0.0.0', port=8483}, function(req,res)
       msg = 'my answer:',
       attached = 'pretty pretty pretty goooood.'
    })
+
+   collectgarbage()
+   print(collectgarbage("count") * 1024)
 end)
 
 async.go()
