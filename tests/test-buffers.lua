8163e0e507adf70e91263d5aee3ae6a6b5759a6e
Clement Farabet <clement.farabet@gmail.com>
2014-06-30 15:08:43 -0400

    testing buffers.

---
+++ tests/test-buffers.lua
@@ -0,0 +1,25 @@
+local async = require 'async'
+local b = require 'buffer'
+
+local server = async.tcp.listen({host='localhost', port=8483}, function(client)
+   client.ondata(function(data)
+      client.write(data)
+   end)
+end)
+
+local client = async.tcp.connect('tcp://localhost:8483/', function(client)
+   client.ondata(function(data)
+      print('received:',data)
+   end)
+
+   local interval = async.setInterval(200, function()
+      client.write(b'test_ontimer')
+   end)
+
+   async.setTimeout(1000, function()
+      client.close()
+      interval.clear()
+   end)
+end)
+
+async.go()
